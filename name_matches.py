ab5bd00920d31ee76e01f30fa9bfd855cdbf1e2a
Rob Redpath <rob.redpath@opendataservices.coop>
2016-10-27 11:06:08 +0100

    [#19] Highlight key fields

--- name_matches.py
+++ name_matches.py
@@ -52,10 +52,19 @@ for key, value in all_names.items():
         recency = item
 
     if len(distinct_data_sources) > 1 or (distinct_name_sources and distinct_data_sources):
-        interesting_names.append((value[0].get('data', {}).get("_recency",""), key, value, info))
+        key_fields = {}
+        key_field_names = ["address","age","name","rest_case_data","gender","Agent","Application Type","Current Status","Decision","Determination Level","Development Description","Status","Proposal","Wards" ]
+        for field_name in key_field_names:
+            try:
+                key_fields[field_name] = value[0]['data'][field_name]
+            except KeyError:
+                pass
+
+        interesting_names.append((value[0].get('data', {}).get("_recency",""), key, value, info, key_fields ))
 
 
 interesting_names.sort(reverse=True)
+
 
 with open("processed/interesting_names.json", "w+") as interesting_names_file:
     json.dump(interesting_names, interesting_names_file, indent=2)
