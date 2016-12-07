821eddd8c4dbf74f814ff95acc32f8ce7cc3b39c
Ben Webb <ben.webb@opendataservices.coop>
2016-12-07 09:14:56 +0000

    Add a simple test of the wards file

--- tests/test_test_data.py
+++ tests/test_test_data.py
@@ -19,3 +19,10 @@ def test_1_match(browser):
     # Click on one of the sources, and there should be a match
     browser.find_element_by_name('sources').click()
     assert 'Matches 1' in browser.find_element_by_tag_name('body').text
+
+
+def test_wards_file(browser):
+    browser.get('file://' + os.path.abspath('./output/wards.html'))
+    assert 'Matches 11' in browser.find_element_by_tag_name('body').text
+    browser.find_element_by_name('sources').click()
+    assert 'Matches 2' in browser.find_element_by_tag_name('body').text
